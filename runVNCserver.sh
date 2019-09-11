2d3dac900d1187b405b17acce37320c42f0fd5e6
Anthony F. Templeton <anthony.f.templeton@nasa.gov>
2019-09-11 10:31:50 -0400

    update runVNCserver.sh script

--- runVNCserver.sh
+++ runVNCserver.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+file="/startapp.sh"
 export DISPLAY=:20
 Xvfb :20 -screen 0 1920x1080x24 &
 x11vnc -display :20 -N -forever -shared -rfbport 5960 &
 /novnc/noVNC/utils/launch.sh --listen 6960 --vnc localhost:5960 &
 openbox-session &
-/startapp.sh
+if [ -f "$file" ]
+then
+  /startapp.sh
+else
+  echo "$file not found"
+fi
