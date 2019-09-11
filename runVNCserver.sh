40bc6464f13bfb9cc72e7c88ec2d92afa200abec
Anthony F. Templeton <anthony.f.templeton@nasa.gov>
2019-09-11 12:09:24 -0400

    update running

--- runVNCserver.sh
+++ runVNCserver.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-file="/startapp.sh"
+file="/runapp.sh"
 export DISPLAY=:20
 Xvfb :20 -screen 0 1920x1080x24 &
 x11vnc -display :20 -N -forever -shared -rfbport 5960 &
 /novnc/noVNC/utils/launch.sh --listen 6960 --vnc localhost:5960 &
-openbox-session &
+if [ -f "$file" ]
+then
+  openbox-session & 
+  /runapp.sh
+else
+  openbox-session
+fi
+
+tail -f /dev/null
