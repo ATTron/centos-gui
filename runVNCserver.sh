f955cebff366109974445f02f5b7bb97e7e7a686
Anthony F. Templeton <anthony.f.templeton@nasa.gov>
2019-09-11 10:09:08 -0400

    inital commit

---
+++ runVNCserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export DISPLAY=:20
+Xvfb :20 -screen 0 1920x1080x24 &
+x11vnc -display :20 -N -forever -shared -rfbport 5960 &
+/novnc/noVNC/utils/launch.sh --listen 6960 --vnc localhost:5960 &
+openbox-session &
+/startapp.sh
